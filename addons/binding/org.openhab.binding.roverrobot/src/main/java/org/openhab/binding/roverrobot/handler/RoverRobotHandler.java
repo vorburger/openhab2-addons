a722caf447eb713c13df90815ee5f1b5c09f053c
Michael Vorburger <mike@vorburger.ch>
2016-10-10 23:22:39 +0200

    RoverRobotHandler more forgiving for channel ID mismatch
    also incl. unrelated attempted dispose() impl

--- addons/binding/org.openhab.binding.roverrobot/src/main/java/org/openhab/binding/roverrobot/handler/RoverRobotHandler.java
+++ addons/binding/org.openhab.binding.roverrobot/src/main/java/org/openhab/binding/roverrobot/handler/RoverRobotHandler.java
@@ -29,8 +29,9 @@ import ch.vorburger.raspberry.turtle.AsyncTurtle;
  */
 public class RoverRobotHandler extends BaseThingHandler {
 
-    private Logger logger = LoggerFactory.getLogger(RoverRobotHandler.class);
+    private Logger log = LoggerFactory.getLogger(RoverRobotHandler.class);
 
+    private TwoMotors twoMotors;
     private AsyncTurtle turtle;
 
     public RoverRobotHandler(Thing thing) {
@@ -39,53 +40,59 @@ public class RoverRobotHandler extends BaseThingHandler {
 
     @Override
     public void handleCommand(ChannelUID channelUID, Command command) {
-        if (channelUID.getId().equals(CHANNEL_1)) {
-            String commandString = command.toString(); // (StringType)
-            switch (commandString) {
-                case "FORWARD2":
-                    turtle.forward(0.2);
-                    break;
-                case "BACK2":
-                    turtle.backward(0.2);
-                    break;
-                case "LEFT45":
-                    turtle.turnLeft(45);
-                    break;
-                case "RIGHT45":
-                    turtle.turnRight(45);
-                    break;
-                default:
-                    logger.warn("handleCommand() unknown command: " + commandString);
-                    break;
-            }
+        if (!channelUID.getId().equals(CHANNEL_1)) {
+            log.warn("handleCommand() on channelUID id {} instead {} (handling anyway)", channelUID.getId(), CHANNEL_1);
+        }
+        String commandString = command.toString(); // (StringType)
+        switch (commandString) {
+            case "FORWARD2":
+                turtle.forward(0.2);
+                break;
+            case "BACK2":
+                turtle.backward(0.2);
+                break;
+            case "LEFT45":
+                turtle.turnLeft(45);
+                break;
+            case "RIGHT45":
+                turtle.turnRight(45);
+                break;
+            default:
+                log.warn("handleCommand() unknown command: " + commandString);
+                break;
         }
     }
 
     @Override
     public void initialize() {
-        logger.info("initialize() is about to set-up Raspberry Pi Rover GPIO....");
+        log.info("initialize() is about to set-up Raspberry Pi Rover GPIO....");
         updateStatus(ThingStatus.INITIALIZING);
 
         try {
-            TwoMotors twoMotors = new TwoMotorsProvider().get();
+            twoMotors = new TwoMotorsProvider().get();
             turtle = new AsyncTurtle(twoMotors);
 
             // Just a hard-coded quick first init test, to signal to the user we're up and ready
             turtle.turnLeft(10);
             turtle.turnRight(10);
 
-            logger.info("initialize() successfully set-up robot turtle control! Ready now.");
+            log.info("initialize() successfully set-up robot turtle control! Ready now.");
             updateStatus(ThingStatus.ONLINE);
 
         } catch (Exception e) {
             updateStatus(ThingStatus.OFFLINE, ThingStatusDetail.HANDLER_INITIALIZING_ERROR, e.getMessage());
-            logger.error("initialize() failed to set-up robot turtle control :(", e);
+            log.error("initialize() failed to set-up robot turtle control :(", e);
         }
     }
 
     @Override
     public void dispose() {
-        // TODO clean release of GPIO .. (This needs a bit of work in ch.vorburger.raspberry.mc33926)
+        turtle.halt();
         turtle = null;
+
+        twoMotors.disable();
+        twoMotors = null;
+
+        // TODO clean release of GPIO .. (This needs a bit of work in ch.vorburger.raspberry.mc33926)
     }
 }
