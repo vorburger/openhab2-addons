30d6f784c2c93afe448a7d2df0f389d5e5dccb1a
Michael Vorburger <mike@vorburger.ch>
2016-10-15 00:59:36 +0200

    RoverRobotHandler with both QueueingTurtle AND AsyncTurtle
    used by the remote control web API; cp openhab-index.html from
    https://github.com/vorburger/rover into openHAB's conf/html/index.html

--- addons/binding/org.openhab.binding.roverrobot/src/main/java/org/openhab/binding/roverrobot/handler/RoverRobotHandler.java
+++ addons/binding/org.openhab.binding.roverrobot/src/main/java/org/openhab/binding/roverrobot/handler/RoverRobotHandler.java
@@ -20,6 +20,7 @@ import org.slf4j.LoggerFactory;
 
 import ch.vorburger.raspberry.mc33926.TwoMotorsProvider;
 import ch.vorburger.raspberry.motors.TwoMotors;
+import ch.vorburger.raspberry.turtle.AsyncTurtle;
 import ch.vorburger.raspberry.turtle.QueueingTurtle;
 import ch.vorburger.raspberry.turtle.Turtle;
 
@@ -33,7 +34,8 @@ public class RoverRobotHandler extends BaseThingHandler {
     private Logger log = LoggerFactory.getLogger(RoverRobotHandler.class);
 
     private TwoMotors twoMotors;
-    private Turtle turtle;
+    private Turtle qTurtle; // QueueingTurtle, best for rulez scripts
+    private Turtle aTurtle; // AsyncTurtle, best for remote control UI
 
     public RoverRobotHandler(Thing thing) {
         super(thing);
@@ -46,51 +48,61 @@ public class RoverRobotHandler extends BaseThingHandler {
         }
         String commandString = command.toString(); // (StringType)
         switch (commandString) {
-            case "HALT":
-                turtle.halt();
-                break;
+            // Commands for the QueueingTurtle, best for rulez scripts
             case "FORWARD2":
-                turtle.forward(0.2);
+                qTurtle.forward(0.2);
                 break;
             case "BACK2":
-                turtle.backward(0.2);
-                break;
-            case "FORWARD4":
-                turtle.forward(0.4);
-                break;
-            case "BACK4":
-                turtle.backward(0.4);
+                qTurtle.backward(0.2);
                 break;
             case "LEFT11":
-                turtle.turnLeft(11.5);
+                qTurtle.turnLeft(11.5);
                 break;
             case "RIGHT11":
-                turtle.turnRight(11.5);
+                qTurtle.turnRight(11.5);
                 break;
             case "LEFT22":
-                turtle.turnLeft(22.5);
+                qTurtle.turnLeft(22.5);
                 break;
             case "RIGHT22":
-                turtle.turnRight(22.5);
+                qTurtle.turnRight(22.5);
                 break;
             case "LEFT45":
-                turtle.turnLeft(45);
+                qTurtle.turnLeft(45);
                 break;
             case "RIGHT45":
-                turtle.turnRight(45);
+                qTurtle.turnRight(45);
                 break;
             case "LEFT90":
-                turtle.turnLeft(90);
+                qTurtle.turnLeft(90);
                 break;
             case "RIGHT90":
-                turtle.turnRight(90);
+                qTurtle.turnRight(90);
                 break;
+
+            // Commands for the AsyncTurtle API, best for remote control UI
+            case "AFORWARD4":
+                aTurtle.forward(0.4);
+                break;
+            case "ABACK4":
+                aTurtle.backward(0.4);
+                break;
+            case "ALEFT90":
+                aTurtle.turnLeft(90);
+                break;
+            case "ARIGHT90":
+                aTurtle.turnRight(90);
+                break;
+            case "AHALT":
+                aTurtle.halt();
+                break;
+
             case "REFRESH":
                 // Ignore what appears to be some sort of well known (but, by us, un-used)
                 // system command, which is frequently sent to us by openHAB core?
                 break;
             default:
-                // log.warn("handleCommand() unknown command: " + commandString);
+                log.warn("handleCommand() unknown command: " + commandString);
                 break;
         }
     }
@@ -102,11 +114,12 @@ public class RoverRobotHandler extends BaseThingHandler {
 
         try {
             twoMotors = new TwoMotorsProvider().get();
-            turtle = new QueueingTurtle(twoMotors);
+            qTurtle = new QueueingTurtle(twoMotors);
+            aTurtle = new AsyncTurtle(twoMotors);
 
             // Just a hard-coded quick first init test, to signal to the user we're up and ready
-            turtle.turnLeft(10);
-            turtle.turnRight(10);
+            qTurtle.turnLeft(10);
+            qTurtle.turnRight(10);
 
             log.info("initialize() successfully set-up robot turtle control! Ready now.");
             updateStatus(ThingStatus.ONLINE);
@@ -121,8 +134,11 @@ public class RoverRobotHandler extends BaseThingHandler {
     public void dispose() {
         log.info("dispose() halting Rover now....");
 
-        turtle.halt();
-        turtle = null;
+        qTurtle.halt();
+        qTurtle = null;
+
+        aTurtle.halt();
+        aTurtle = null;
 
         twoMotors.disable();
         twoMotors = null;
